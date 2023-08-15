1ec3067fdf9efe5b48ba0bce7d85aa61aeed9ecb
Qi He <heqi@hyperchain.cn>
2023-08-15 16:42:56 +0800

    update comment

--- generate_re.py
+++ generate_re.py
@@ -1,5 +1,5 @@
 from tts import get_one_voice_with_ssml
 
 if __name__ == '__main__':
-    # get_one_voice_with_ssml('1-3_files/p_4.xml', '1-3_files/video_4.wav')
-    get_one_voice_with_ssml('p_3.xml', 'video_3.wav')
+    get_one_voice_with_ssml('3-4_files/p_7.xml', '3-4_files/video_7.wav')
+    # get_one_voice_with_ssml('p_3.xml', 'video_3.wav')
